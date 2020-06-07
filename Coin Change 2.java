eb8af34c29bb5a6a356de9b003e72cce101b8e8e
Priya Bhatia <priya.bhatia6971@gmail.com>
2020-06-07 22:10:51 +0530

    Add files via upload

---
+++ Coin Change 2.java
@@ -0,0 +1,40 @@
+/* HashMap is used to avoid Overlapping Subproblem */
+
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        
+        HashMap<String,Integer> hm = new HashMap<>();
+        
+        return helper(amount,coins,0,hm);
+        
+    }
+    
+    private int helper(int amount,int[] coins,int index,HashMap<String,Integer> hm){
+        if(amount == 0){
+            return 1;
+        }
+        
+        if(amount < 0 || index == coins.length){
+            return 0;
+        }
+        
+        String key = amount + "code" + index;
+        
+        if(hm.containsKey(key)){
+            return hm.get(key);
+        }
+        
+        int result = 0;
+        
+        for(int i=index;i<coins.length;i++){
+            
+            if(amount >= coins[i]){
+                result = result + helper(amount-coins[i],coins,i,hm);
+            }
+            
+        }
+        hm.put(key,result);
+        return result;
+    }
+}
