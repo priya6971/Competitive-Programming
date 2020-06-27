afaab36c8e62767e3171ec35a753eca5b393cd26
Priya Bhatia <priya.bhatia6971@gmail.com>
2020-06-27 22:38:09 +0530

    Add files via upload

---
+++ Perfect Squares.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numSquares(int n) {
+        int memo[] = new int[n+1];
+        return rec(n,memo);
+    }
+    
+    public int rec(int n,int memo[]){
+        if(n < 0)
+            return Integer.MAX_VALUE;
+        if(n == 0)
+            return 0;
+        if(memo[n] > 0)
+            return memo[n];
+        
+        int min = n;
+        
+        for(int i=1;i*i<=n;i++){
+            min = Math.min(rec(n-(i*i),memo),min);
+        }
+        
+        memo[n] = min+1;
+        return min+1;
+    }  
+}
+    
